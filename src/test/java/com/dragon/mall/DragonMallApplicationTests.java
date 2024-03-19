2bdbc66dbab9d8ef58ab972f8fbdbd0c4229de36
赌狗 <2737550037@qq.com>
2024-03-19 23:22:43 +0800

    init: 🎉 项目初始化

---
+++ src/test/java/com/dragon/mall/DragonMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
